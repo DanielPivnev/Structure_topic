dca20d4b537463f04b4d07e1ec87ecefad132adb
DanielPivnev <d.pivnev2003@gmail.com>
2021-07-03 20:40:06 +0200

    finished

---
+++ dz2.py
@@ -0,0 +1,11 @@
+import collections
+
+
+hexa_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+a = input('Pleas enter: ').upper()
+b = input('Pleas enter: ').upper()
+
+max_len = max(len(a), len(b))
+for i in range(max_len):
+
+
