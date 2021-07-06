0472c2838a0f796daa6ab1c39e33f7fd84ab7d4f
DanielPivnev <d.pivnev2003@gmail.com>
2021-07-06 19:07:38 +0200

    finished

--- dz8.py
+++ dz8.py
@@ -9,16 +9,15 @@ for i in range(5):
     ls2.append(sum(ls2))
     ls1.append(ls2)
 
-max_lengths = [0, 0, 0, 0]
-i = 0
-for element in zip(ls1[0], ls1[1], ls1[2], ls1[3], ls1[4]):
-    for j in range(len(element)):
-        temporary_max_len = len(str(element[j]))
-        if temporary_max_len > max_lengths[i]:
-            max_lengths[i] = temporary_max_len
-    i += 1
+max_lengths = []
+max_len = 0
+for list_2 in zip(ls1[0], ls1[1], ls1[2], ls1[3], ls1[4]):
+    for j in range(len(list_2)):
+        if len(str(list_2[j])) > max_len:
+            max_len = len(str(list_2[j]))
+    max_lengths.append(max_len)
 
-print('+—   ' + (' ' * sum(max_lengths)) + '—+')
+print('+—-' + (' ' * sum(max_lengths)) + ' -—+')
 for i in range(5):
     print('| ', end='')
     for j in range(4):
@@ -26,6 +25,6 @@ for i in range(5):
             if type(ls1[i][j]) != str:
                 ls1[i][j] = str(ls1[i][j])
             ls1[i][j] = ' ' + ls1[i][j]
-        print(ls1[i][j], ' ', end='')
+        print(ls1[i][j], ' ', end='', sep='')
     print('|')
-print('+—   ' + (' ' * sum(max_lengths)) + '—+')
+print('+—-' + (' ' * sum(max_lengths)) + ' -—+')
